de7289cc61eaa3a8720fb6c4a27f33b17a22035a
LiuYinCarl <bearcarl@qq.com>
2023-11-04 01:46:48 +0800

    add plugins manager

---
+++ emacs/manager_git_package.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+############################################################
+## Emacs Git Package Manage Script                        ##
+##   Once you edit this script, use shellcheck to check   ##
+##   the changes.                                         ##
+############################################################
+
+############################################################
+## User Config
+############################################################
+
+# where you want to save the git repos
+repo_dir="${HOME}/.emacs.d/plugins"
+
+# which repos you want to manage
+repos=(
+    "git@github.com:manateelazycat/awesome-tab.git"       \
+    "git@github.com:tarsius/hl-todo.git"                  \
+    "git@github.com:scottjad/parenface.git"               \
+    "git@github.com:redguardtoo/find-file-in-project.git" \
+    "git@github.com:gonewest818/dimmer.el.git"            \
+    "git@github.com:emacs-vs/goto-line-preview.git"       \
+    "git@github.com:joaotavora/breadcrumb.git"            \
+    "git@github.com:jdtsmith/indent-bars.git"             \
+    "git@github.com:manateelazycat/auto-save.git"         \
+    "git@github.com:manateelazycat/delete-block.git"
+)
+
+############################################################
+## Function
+############################################################
+
+function update_current_dir_repos() {
+    git_repo_cnt=0
+
+    for dir in *; do
+        if [ -d "${dir}" ]; then
+            cd "${dir}" || exit
+            if [ -d ".git" ]; then
+                ((git_repo_cnt += 1))
+                cur_dir=$(pwd)
+                echo "[${git_repo_cnt}] start pull ${cur_dir}..."
+                time git pull
+            fi
+            cd ..
+        fi
+    done
+}
+
+function run() {
+    if [ "${1}" = "ls" ]; then
+        dir_path=$(pwd)
+        dir_size=$(du -sh | cut -f 1)
+        echo "repo_dir_path: ${dir_path}"
+        echo "repo_dir_size: ${dir_size}"
+        echo ""
+        ls
+    elif [ "${1}" = "backup" ]; then
+        echo "start backup..."
+        name=repo_backup_$(date +%Y%m%d-%H%M%S).tar.gz
+        tar -czf "${name}" ./
+        printf "backup file: $%s/%s\n" "$(pwd)" "${name}"
+    elif [ "${1}" = "clear" ]; then
+        rm repo_backup_*
+    elif [ "${1}" = "clone" ]; then
+        for repo in "${repos[@]}"
+        do
+            git clone --depth=1 "${repo}"
+        done
+    elif [ "${1}" = "pull" ]; then
+        update_current_dir_repos
+    else
+        echo "Script Args:"
+        echo "    ls      list repo directory"
+        echo "    backup  backup repo directory"
+        echo "    clear   remove all backup"
+        echo "    clone   clone all uninstalled repo"
+        echo "    pull    update all installed repo"
+    fi
+}
+
+############################################################
+## Start
+############################################################
+
+mkdir -p "${repo_dir}"
+cd "${repo_dir}" || exit
+
+printf "\e[32m" # set color
+
+run "$@"
+
+printf "\e[0m"  # reset color
+
